e1490e5afee438c6f5c73b85eef7a2ffa3538ca2
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2023-08-22 08:56:53 +0700

    Day 6 Project update

--- exercises/day-6-exercise-reborg-1.py
+++ exercises/day-6-exercise-reborg-1.py
@@ -1,22 +1,22 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%201&url=worlds%2Ftutorial_en%2Fhurdle1.json
 # this code is used on reeborg website above
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-
-def jump():
-    move()
-    turn_left()
-    move()
-    turn_right()
-    move()
-    turn_right()
-    move()
-    turn_left()
-
-
-
-for step in range(6):
-    jump()
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+#
+# def jump():
+#     move()
+#     turn_left()
+#     move()
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#     turn_left()
+#
+#
+#
+# for step in range(6):
+#     jump()
--- exercises/day-6-exercise-reborg-4.py
+++ exercises/day-6-exercise-reborg-4.py
@@ -1,25 +1,25 @@
 # https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%204&url=worlds%2Ftutorial_en%2Fhurdle4.json
 # this code is used on reeborg website above
-
-def turn_right():
-    turn_left()
-    turn_left()
-    turn_left()
-
-def jump():
-    turn_right()
-    move()
-    turn_right()
-    move()
-
-while not at_goal():
-    if is_facing_north() and wall_in_front():
-        jump()
-    elif wall_in_front():
-        turn_left()
-    elif is_facing_north() and wall_on_right():
-        move()
-    elif is_facing_north() and not wall_on_right():
-        jump()
-    elif not wall_in_front():
-        move()
+#
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+#
+# def jump():
+#     turn_right()
+#     move()
+#     turn_right()
+#     move()
+#
+# while not at_goal():
+#     if is_facing_north() and wall_in_front():
+#         jump()
+#     elif wall_in_front():
+#         turn_left()
+#     elif is_facing_north() and wall_on_right():
+#         move()
+#     elif is_facing_north() and not wall_on_right():
+#         jump()
+#     elif not wall_in_front():
+#         move()
