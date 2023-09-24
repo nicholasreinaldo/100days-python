48d99af2e09a1ab4f7a041a01e5c530ab4769fa1
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2023-09-24 23:03:33 +0700

    Day 8 Project update

---
+++ exercises/day-8/main.py
@@ -0,0 +1,12 @@
+# def greet_with_name(name):
+#     print(f"Hello {name}")
+#     print(f"How do you do {name}")
+#     print("Isn't the weather nice today?")
+#
+# greet_with_name("Billie")
+
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}?")
+
+greet_with("Jack Bauer", "California")
