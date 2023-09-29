6f236328e67824ce52c2c8294e3d8d4234d86eb6
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2023-09-29 18:41:45 +0700

    Day 9 Project update

--- exercises/exercise.py
+++ exercises/exercise.py
@@ -1,3 +1,19 @@
-ea = dict(zip([1],["a"]))
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
 
-print(ea)
+# Access the "5" in the second dictionary
+countries = []
+for visit in travel_log:
+    countries.append(travel_log[1]["country"])
+
+print(countries)
--- projects/day_9_blind_auction/blind-auction.py
+++ projects/day_9_blind_auction/blind-auction.py
@@ -11,7 +11,6 @@ def new_bid(input_name, input_bid):
     new_bidder = {
         "bidder_name": input_name,
         "bid_amount": input_bid,
-
     }
     bid_pool.append(new_bidder)
 
@@ -19,10 +18,15 @@ bidding = True
 while bidding:
     name = input("What is your name?: ")
     bid = input("What is your bid?: $")
-    more_bidders = input("Are there any other bidders? Type 'yes or no'"))
+    more_bidders = input("Are there any other bidders? Type 'yes or no' \n")
     new_bid(input_name=name, input_bid=bid)
     if more_bidders == "no":
         bidding = False
-        print(f"The winner is {} with a bid of ${}")
-
-
+        bid_amounts = []
+        for bidder in bid_pool:
+            bid_amounts.append(bidder["bid_amount"])
+        highest_bid_amount = max(bid_amounts)
+        highest_bidder_name = bid_pool[bid_amounts.index(highest_bid_amount)]["bidder_name"]
+        print(f"The winner is {highest_bidder_name} with a bid of ${highest_bid_amount}")
+    elif more_bidders == "yes":
+        clear()
