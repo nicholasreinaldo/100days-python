a9ddb6464038fbc1daf07e9592f27669e2ae58b2
Nicholas Reinaldo <nicholasreinaldo27@gmail.com>
2023-10-01 20:45:24 +0700

    Day 9 Project update

---
+++ exercises/day-10/day-10-exercise.py
@@ -0,0 +1,12 @@
+# Functions with Outputs
+length = len(formatted_name)
+def format_name(f_name, l_name):
+    """Take a first and last name and format it
+    return the title case version of the name."""
+    if f_name == "" or l_name == "":
+        return
+    formated_f_name = f_name.title()
+    formated_l_name = l_name.title()
+    return f"{formated_f_name} {formated_l_name}"
+
+print(format_name("nicholas", "reinaldo"))
